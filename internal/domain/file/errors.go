33aea9cba4e0c7726af8a8016725d2b8b95d40ce
Lapp <lapp-coder@mail.ru>
2022-05-04 12:34:25 +0300

    internal/domain/file: remove unused error

--- internal/domain/file/errors.go
+++ internal/domain/file/errors.go
@@ -5,5 +5,4 @@ import "errors"
 var (
 	ErrFailedToSaveFile               = errors.New("failed to save file")
 	ErrFailedToGetFileByUUID          = errors.New("failed to get file by uuid")
-	ErrFailedToGetFileStatisticByUUID = errors.New("failed to get file statistic by uuid")
 )
